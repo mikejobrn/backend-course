30dc0540e107328b664cc43e46081c6a6d5fbcde
Hamurabi Araujo <araujohamurabi@gmail.com>
2024-08-06 10:31:03 -0300

    Evolução nos casos de teste do CRUD de usuarios
    Co-authored-by: Hugolobo22 <Hugolobo22@users.noreply.github.com>
    Co-authored-by: Lucinderier <Lucinderier@users.noreply.github.com>
    Co-authored-by: julius <juliuspng@users.noreply.github.com>
    Co-authored-by: PedroVictor0 <PedroVictor0@users.noreply.github.com>
    Co-authored-by: SpruceOak <Donut6466@users.noreply.github.com>
    Co-authored-by: YuriCruzz <YuriCruzz@users.noreply.github.com>

--- aulas/aula19/projeto/src/routes/usuario/usuario.test.ts
+++ aulas/aula19/projeto/src/routes/usuario/usuario.test.ts
@@ -2,7 +2,7 @@ import request, { Response } from "supertest";
 import app from "../../app";
 
 describe("Testa a rota de usuários", () => {
-  test("Deve responder o método GET com sucesso", done => {
+  test("Deve responder o método GET com sucesso", (done) => {
     request(app)
       .get("/usuarios")
       .then((response: Response) => {
@@ -11,7 +11,7 @@ describe("Testa a rota de usuários", () => {
       });
   });
 
-  test("Deve responder o método GET com uma lista de 10 registros", done => {
+  test("Deve responder o método GET com uma lista de 10 registros", (done) => {
     request(app)
       .get("/usuarios")
       .then((response: Response) => {
@@ -20,7 +20,7 @@ describe("Testa a rota de usuários", () => {
       });
   });
 
-  test("Deve retornar um registro com sucesso", done => {
+  test("Deve retornar um registro com sucesso", (done) => {
     request(app)
       .get("/usuarios/10")
       .then((response: Response) => {
@@ -28,6 +28,34 @@ describe("Testa a rota de usuários", () => {
         expect(response.body).not.toBeNull();
         expect(response.body).toBeDefined();
         expect(response.body.id).toBe(10);
+        done();
+      });
+  });
+
+  test("Deve deletar um registro com sucesso", (done) => {
+    request(app)
+      .delete("/usuarios/10")
+      .then((response: Response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+
+  test("Deve criar um novo usuário com sucesso", (done) => {
+    const newUser = {
+      nome: "Novo Usuário",
+      login: "novousuario",
+      senha: "senha000",
+    };
+
+    request(app)
+      .post("/usuarios")
+      .send(newUser)
+      .then((response: Response) => {
+        expect(response.statusCode).toBe(201);
+        expect(response.body).not.toBeNull();
+        expect(response.body.login).toBe(newUser.login);
+        expect(response.body.id).not.toBeUndefined();
         done();
       });
   });
